b25018a3d3e030b17d14e230bec99d5fb13d2694
Seung Bin Lee <sl3948@columbia.edu>
2018-11-28 23:52:24 -0500

    Update Travis configuration.

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -9,7 +9,6 @@ import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
 import createElectronStorage from 'redux-persist-electron-storage';
 
 import createRootReducer from '../reducers';
-import * as counterActions from '../actions/counter';
 import type { counterStateType } from '../reducers/types';
 
 const history = createHashHistory();
@@ -53,7 +52,6 @@ const configureStore = (initialState?: counterStateType) => {
 
   // Redux DevTools Configuration
   const actionCreators = {
-    ...counterActions,
     ...routerActions
   };
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
